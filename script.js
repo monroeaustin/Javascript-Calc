3c896854a4308cd0557836ca24ddbd92ae9ae533
Monroe Austin <monroeraustin2@yahoo.com>
2024-05-06 19:07:53 -0700

    Created functions for calc logic

---
+++ script.js
@@ -0,0 +1,21 @@
+// Creating Functions for Calc
+
+const add = function (num,newNum) {
+    // num is the pressed number// newNum is the number pressed after applying '+"
+    return  num + newNum
+    
+}
+
+const subtract = function (num,newNum){
+    return num - newNum
+}
+
+const multiply = function (num,newNum){
+    return num * newNum
+}
+
+const divide = function (num,newNum){
+    return num * newNum
+}
+
+// Functions End Here
