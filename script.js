e2db2d30ec1898f4a94d8b2a9af4a0a6a6aad8b7
Monroe Austin <monroeraustin2@yahoo.com>
2024-05-06 20:08:04 -0700

    Modified Calc Buttons to correct button syntax

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 // Creating Functions for Calc
 
 const add = function (num,newNum) {
-    // num is the pressed number// newNum is the number pressed after applying '+"
-    return  num + newNum
+    // num is the pressed number// newNum is the number pressed after applying 'the operator"
+    const totalNum = num + newNum
+    return  totalNum
     
 }
 
@@ -19,3 +20,20 @@ const divide = function (num,newNum){
 }
 
 // Functions End Here
+
+let operator
+let num 
+let newNum
+
+function operate (operator,num,newNum) {
+    if (operator === 'add'){
+        add(num,newNum)
+        
+        sum = num + newNum
+        return sum
+    }
+
+    else {
+        console.log('try-again')
+    }
+}
